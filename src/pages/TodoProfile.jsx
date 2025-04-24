9202a784e85adf91fc01b33cf2a177d3cf56b450
Hyuna Kang <kho8815@gmail.com>
2025-04-24 19:34:33 +0900

    feat(tab): add tab navigation
    - 탭 선택 시 해당 화면 렌더링 (스타일 미적용)
    - store/useTabStore.js : 탭 상태관리 (zustand)

--- src/pages/TodoProfile.jsx
+++ src/pages/TodoProfile.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
 
 import Header from '../components/Header.jsx';
+import useTabStore from '../store/useTabStore.js';
 
 function TodoProfile() {
+  const { currentTab } = useTabStore();
   return (
     <div className="page-container">
       <Header title="PROFILE" />
 
       {/*작업시 삭제후 진행해주세요*/}
       <h1>프로필 페이지</h1>
-      {/*작업시 삭제후 진행해주세요*/}
+      <TabNav />
+      <div>
+        {currentTab === '할 일' && <TodoList />}
+        {currentTab === '게시글' && <Posts />}
+      </div>
     </div>
   );
 }
 
 export default TodoProfile;
+
+const tabs = ['할 일', '게시글'];
+const TabNav = () => {
+  const { currentTab, setCurrentTab } = useTabStore();
+
+  return (
+    <div>
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          onClick={() => setCurrentTab(tab)}
+          className={currentTab === tab ? 'currentTab' : ''}
+        >
+          {tab}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+const TodoList = () => {
+  return <div>TodoList</div>;
+};
+const Posts = () => {
+  return <div>Posts</div>;
+};
---
+++ src/store/useTabStore.js
@@ -0,0 +1,9 @@
+// 프로필 페이지 내 [할 일 기록, 게시물] 탭 상태관리
+import { create } from 'zustand';
+
+const useTabStore = create((set) => ({
+  currentTab: '할 일', // 초기 탭 설정
+  setCurrentTab: (tab) => set({ currentTab: tab })
+}));
+
+export default useTabStore;
