b35dd81a77fbac6ec5ca5d222d42dabe3d0d4d69
sjoon627 <sjoon627@naver.com>
2025-04-25 11:16:04 +0900

    feat: 아바타 이미지 추가 기본 구조 작성

--- src/pages/TodoProfile.jsx
+++ src/pages/TodoProfile.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
+import avatarImg from '../assets/avatar.svg';
 import Header from '../components/Header.jsx';
 
 function TodoProfile() {
   return (
     <div className="page-container">
       <Header title="PROFILE" />
+      <div className="profile-container">
+        <div className="avatar-container">
+          <img src={avatarImg} alt="Profile Avatar" className="profile-avatar" />
+        </div>
+        <h2 className="profile-name">GrowLog</h2>
 
-      {/*작업시 삭제후 진행해주세요*/}
-      <h1>프로필 페이지</h1>
-      {/*작업시 삭제후 진행해주세요*/}
+        <div className="profile-tabs">
+          <button className="tab-button active">할일 기록</button>
+          <button className="tab-button">게시물</button>
+        </div>
+
+        <div className="tab-content">
+          <div className="tasks-container">{/* 할일 기록 내용이 들어갈 자리 */}</div>
+          <div className="posts-container">{/* 게시물 내용이 들어갈 자리 */}</div>
+        </div>
+      </div>
     </div>
   );
 }
