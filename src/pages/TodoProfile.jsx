139405bbd08550d70bb6aab1e7daa67f57ef8329
Hyuna Kang <kho8815@gmail.com>
2025-04-25 16:10:57 +0900

    docs: move file directory(usetabstore.js)

--- src/pages/TodoProfile.jsx
+++ src/pages/TodoProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import Header from '../components/Header.jsx';
-import useTabStore from '../store/useTabStore.js';
+import useTabStore from '../stores/useTabStore.js';
 
 function TodoProfile() {
   const { currentTab } = useTabStore();
