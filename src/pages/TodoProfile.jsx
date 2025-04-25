8b84715f2d420c766d31d33e9e693b3ea4f8f7f8
sjoon627 <sjoon627@naver.com>
2025-04-25 14:03:46 +0900

    feat:  페이지의 할일 및 게시물 섹션 구조 생성 및 스타일 수정

--- src/pages/TodoProfile.jsx
+++ src/pages/TodoProfile.jsx
@@ -14,15 +14,39 @@ function TodoProfile() {
           <h2 className="profile-name">GrowLog</h2>
         </div>
       </div>
+
       <div className="tab-contianer">
         <div className="profile-tabs">
-          <button className="tab-button active">할일 기록</button>
+          <button className="tab-button currentTab">할일 기록</button>
           <button className="tab-button">게시물</button>
         </div>
 
         <div className="tab-content">
-          <div className="tasks-container">{/* 할일 기록 내용이 들어갈 자리 */}</div>
-          <div className="posts-container">{/* 게시물 내용이 들어갈 자리 */}</div>
+          <div className="todo-container">
+            <div className="todo-doing">
+              <span className="todo-count doing">해야할 일</span>
+
+              <div className="todo-item">
+                <div className="priority-circle important"></div>
+                <span>TODO 1 TODO 1 TODO 1 TODO 1 TODO 1 ...</span>
+              </div>
+
+              <div className="todo-item">
+                <div className="priority-circle normal"></div>
+                <span>할일 2</span>
+              </div>
+            </div>
+
+            <div className="todo-done">
+              <span className="todo-count done">완료한 일</span>
+              <div className="todo-item">
+                <div className="priority-circle none"></div>
+                <span>할일 3</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="post-container">{/* 게시물 내용이 들어갈 자리 */}</div>
         </div>
       </div>
     </div>
