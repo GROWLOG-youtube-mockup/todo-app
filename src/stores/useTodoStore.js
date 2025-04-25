b7965647e957cfcd45e3906d9682262796742c56
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-25 09:38:44 +0900

    feat: store에 할 일 수정 기능 추가

--- src/stores/useTodoStore.js
+++ src/stores/useTodoStore.js
@@ -15,6 +15,21 @@ export const useTodoStore = create(
         priority
       };
       set({ todos: [...get().todos, newTodo] });
+    },
+
+    updateTodo: ({ id, title, description, isComplete, priority }) => {
+      const updatedTodos = get().todos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              ...(title !== undefined && { title }),
+              ...(description !== undefined && { description }),
+              ...(isComplete !== undefined && { isComplete }),
+              ...(priority !== undefined && { priority })
+            }
+          : todo
+      );
+      set({ todos: updatedTodos });
     }
   }))
 );
