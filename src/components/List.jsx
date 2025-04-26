2d8a3cb76c048f511c167aaa0a261bd2af1c3dc5
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-26 15:36:08 +0900

    fix: todo list에 매개변수 추가함

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,8 +5,14 @@ import TodoItem from './TodoItem.jsx';
 function List({ todos }) {
   return (
     <ul>
-      {todos.map(({ id, title, description, priority }) => (
-        <TodoItem key={id} todo={title} description={description} priority={priority} />
+      {todos.map(({ id, title, description, priority, isComplete }) => (
+        <TodoItem
+          key={id}
+          todo={title}
+          description={description}
+          priority={priority}
+          isComplete={isComplete}
+        />
       ))}
     </ul>
   );
